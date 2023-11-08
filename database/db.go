5152b52189a92964d03d5ae27082c44a78d57676
Zach <zap.freelance@gmail.com>
2023-11-08 21:36:44 +0000

    changed database connection

--- database/db.go
+++ database/db.go
@@ -14,7 +14,7 @@ var DB *gorm.DB
 
 func init() {
 	var err error
-	dsn := os.Getenv("POSTGRE_DSN")
+	dsn := os.Getenv("DATABASE_URL")
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
